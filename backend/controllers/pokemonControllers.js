0bd4da45f17b705d9621d6cb0e7b8d2c7df400af
Daniele <daniele.capa@gmail.com>
2024-06-05 11:33:40 +0200

    fixed nested fetch

--- backend/controllers/pokemonControllers.js
+++ backend/controllers/pokemonControllers.js
@@ -22,13 +22,25 @@ FIXME: // get pokemon info / name, type, base ??????
 
 exports.getPokemonInfo = async (req, res) => {
     const id = parseInt(req.params.id, 10);
-    const info = req.params.info;
+    const infoPath = req.params[0];
+
     const pokemon = pokedex.find(p => p.id === id);
-    if (pokemon && pokemon[info]) {
-      res.json(pokemon[info]);
-    } else {
-      res.status(404).send('Pokemon or information not found');
+
+    if (!pokemon) {
+        return res.status(404).send("Pokemon not found");
     }
-  };
-    
+
+    const infoParts = infoPath.split('/');
+    let infoValue = pokemon;
+
+    for (const part of infoParts) {
+        if (infoValue[part] !== undefined) {
+            infoValue = infoValue[part];
+        } else {
+            return res.status(404).send("Info not found");
+        }
+    }
+
+    return res.send({ [infoParts[infoParts.length - 1]]: infoValue });
+};
 
