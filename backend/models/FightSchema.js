09ccd60d6e812e739fcf6cd1c4a22f07983984d3
hamed <raahnaward@gmail.com>
2024-06-17 17:36:24 +0200

    last commit

--- backend/models/FightSchema.js
+++ backend/models/FightSchema.js
@@ -1,18 +1,3 @@
-/* const mongoose = require('mongoose');
-
-const fightResultSchema = new mongoose.Schema({
-  Pokemon1: String,
-  Pokemon2: String,
-  battleLog: [String],
-  winner: String
-});
-
-const FightResult = mongoose.model('Fight', fightResultSchema);
-
-module.exports = FightResult;
- */
-// models/fightSchema.js
-
 const mongoose = require('mongoose');
 
 const fightSchema = new mongoose.Schema({
