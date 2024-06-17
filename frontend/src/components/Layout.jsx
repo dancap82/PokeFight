677fb6cecbf4c4059f12fc49c806b8e50ac010e5
YagoPazos <yagopb@gmail.com>
2024-06-17 18:01:04 +0200

    last commit

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,27 +1,59 @@
 import { NavLink, Outlet } from "react-router-dom";
-import logo from '../assets/imgs/pokemon-23.svg';
+import logo from "../assets/imgs/pokemon-23.svg";
+import facebook from "../assets/icons/facebook-50.png";
+import instagram from "../assets/icons/instagram-50.png";
+import tiktok from "../assets/icons/tiktok-50.png";
+import youtube from "../assets/icons/youtube-50.png";
 import "../App.css";
-
 
 function Layout() {
   return (
     <>
-      <nav
-        className="flex nav justify-between py-4 px-6 md:px-20 lg:px-28 xl:px-36 2xl:px-60 items-center"
-      >
+      <nav className="flex nav justify-between py-4 px-6 md:px-12 lg:px-20 xl:px-24 2xl:px-32 items-center">
         <img className="w-32" src={logo} alt="pokemon-logo" />
-        <div
-          className="flex align-middle gap-4 md:gap-8 lg:gap-20 xl:gap-32 2xl:gap-40"
-        >
-          <NavLink className="navlinks" to="/"> Home </NavLink>
-          <NavLink className="navlinks" to="/pokedex"> Pokédex </NavLink>
-          <NavLink className="navlinks" to="/fight"> Fight </NavLink>
-          <NavLink className="navlinks" to="/FightResults"> Fight Results </NavLink>
+        <div className="flex align-middle gap-4 md:gap-6 lg:gap-10 xl:gap-20 2xl:gap-32">
+          <NavLink className="navlinks" to="/">
+            {" "}
+            Home{" "}
+          </NavLink>
+          <NavLink className="navlinks" to="/pokedex">
+            {" "}
+            Pokédex{" "}
+          </NavLink>
+          <NavLink className="navlinks" to="/fight">
+            {" "}
+            Fight{" "}
+          </NavLink>
+          <NavLink className="navlinks" to="/FightResults">
+            {" "}
+            Fight Results{" "}
+          </NavLink>
         </div>
       </nav>
       <main>
         <Outlet />
       </main>
+      <footer className="bg-black flex flex-col md:flex-row p-4 text-center md:text-left justify-around">
+        <img className="w-80 mx-auto md:mx-0" src={logo} alt="logo-pokemon" />
+        <div className="flex flex-col justify-center mt-4 md:mt-0 md:items-start">
+          <br />
+          <h4 className="text-white font-bold">Company</h4>
+          <br />
+          <p className="text-white">Kopernikusstraße 16</p>
+          <p className="text-white">15430, Berlin</p>
+          <p className="text-white">Germany</p>
+        </div>
+        <div className="flex flex-col pt-12 md:pt-0 justify-center">
+          <h4 className="text-white font-bold">Contact Us</h4>
+          <br />
+          <div className="flex justify-center md:justify-start gap-2">
+            <img src={facebook} alt="facebook-logo" />
+            <img src={instagram} alt="instagram-logo" />
+            <img src={tiktok} alt="tiktok-logo" />
+            <img src={youtube} alt="youtube-logo" />
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,10 +1,4 @@
 import "../App.css";
-import logowhite from "../assets/imgs/pokemon-logo-white.svg";
-import logo from "../assets/imgs/pokemon-23.svg";
-import facebook from "../assets/icons/facebook-50.png";
-import instagram from "../assets/icons/instagram-50.png";
-import tiktok from "../assets/icons/tiktok-50.png";
-import youtube from "../assets/icons/youtube-50.png";
 import Cards from "./Cards";
 
 function Main() {
@@ -23,27 +17,6 @@ function Main() {
         </div>
       </section>
       <Cards />
-      <footer className="bg-black flex flex-col md:flex-row p-4 text-center md:text-left justify-around">
-        <img className="w-80 mx-auto md:mx-0" src={logo} alt="logo-pokemon" />
-        <div className="flex flex-col justify-center mt-4 md:mt-0 md:items-start">
-          <br />
-          <h4 className="text-white font-bold">Company</h4>
-          <br />
-          <p className="text-white">Kopernikusstraße 16</p>
-          <p className="text-white">15430, Berlin</p>
-          <p className="text-white">Germany</p>
-        </div>
-        <div className="flex flex-col pt-12 md:pt-0 justify-center">
-          <h4 className="text-white font-bold">Contact Us</h4>
-          <br />
-          <div className="flex justify-center md:justify-start gap-2">
-            <img src={facebook} alt="facebook-logo" />
-            <img src={instagram} alt="instagram-logo" />
-            <img src={tiktok} alt="tiktok-logo" />
-            <img src={youtube} alt="youtube-logo" />
-          </div>
-        </div>
-      </footer>
     </>
   );
 }
